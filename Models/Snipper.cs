42a11b27fe986d6f46821947e97b93945349db9a
Bilal Mohamed-Benkada <bmbenkada@gmail.com>
2024-01-18 13:19:31 +0000

    Created Snipper model

---
+++ Models/Snipper.cs
@@ -0,0 +1,9 @@
+namespace SnipperAPI
+{
+    public partial class SnipperAPI
+    {
+        public int ItemId { get; set; } // Might not be necessary
+        public string Language { get; set; } = "";
+        public string Code { get; set; } = "";
+    }
+}
